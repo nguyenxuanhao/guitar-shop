7b82e6cc56560618380dc409ecd52c85ed5e85b8
nxhao <nxhao235@gmail.com>
2019-05-15 11:23:34 +0700

    add thymeleaf

---
+++ src/main/java/org/group02/guitarshop/GuitarShopApplication.java
@@ -0,0 +1,13 @@
+package org.group02.guitarshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuitarShopApplication.class, args);
+    }
+
+}
