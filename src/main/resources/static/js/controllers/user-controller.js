22ee7d446e8cc77fb99f3a8417aab6ca52f2db69
nxhao <nxhao235@gmail.com>
2019-06-18 22:38:41 +0700

    update registration

--- src/main/resources/static/js/controllers/user-controller.js
+++ src/main/resources/static/js/controllers/user-controller.js
@@ -1,36 +1,4 @@
-﻿var dialog = document.querySelector('dialog');
-
-function fnRegisterSuccess(res) {
-        dialog.showModal();
-}
-$("#registrationForm").submit(function(e) {
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
-
-    var form = $(this);
-
-    $.ajax({
-        type: "POST",
-        url: "/dang-ky",
-        data: form.serialize(), // serializes the form's elements.
-        success: function(data)
-        {
-            alert(data); // show response from the php script.
-        }
-    });
-});
-
-
-function fnLoginSuccess(res) {
-    if (res.result == true) {
-        window.location.href = "/";
-    }
-    if (res.result == false) {
-        window.location.href = "/thanh-toan";
-    }
-}
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     $(".closeDialog").on("click", function () {
         dialog.close();
     })
